df3a672fd295f16be9e9c58faa3ab37b8e85e5b8
JustTrying652 <ngataevans@gmail.com>
2026-07-18 10:29:58 +0300

    add packages

---
+++ src/theme/index.ts
@@ -0,0 +1,25 @@
+export const colors = {
+  ink: '#14161A',
+  paper: '#F7F3EC',
+  paperMuted: '#EDE8DD',
+  stampGreen: '#0F8B46',
+  receiptRed: '#B3261E',
+  kangaGold: '#D8A31A',
+  ash: '#6B6F76',
+  white: '#FFFFFF',
+};
+
+export const fonts = {
+  display: 'SpaceGrotesk_700Bold',
+  displayMedium: 'SpaceGrotesk_500Medium',
+  mono: 'JetBrainsMono_400Regular',
+  monoBold: 'JetBrainsMono_700Bold',
+};
+
+export const spacing = {
+  xs: 4,
+  sm: 8,
+  md: 16,
+  lg: 24,
+  xl: 32,
+};
