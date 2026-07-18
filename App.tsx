75f5c972b33b5ea791666e288dad5ddbd903fadc
JustTrying652 <ngataevans@gmail.com>
2026-07-18 10:13:23 +0300

    wire in new screens

--- App.tsx
+++ App.tsx
@@ -1,16 +1,27 @@
 import { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { seedCategoriesIfEmpty } from './src/db/storage';
+import { seedCategoriesIfEmpty, getHasOnboarded } from './src/db/storage';
 import RootNavigator from './src/navigation';
+import OnboardingScreen from './src/screens/OnboardingScreen';
 
 export default function App() {
   const [ready, setReady] = useState(false);
+  const [showOnboarding, setShowOnboarding] = useState(false);
 
   useEffect(() => {
-    seedCategoriesIfEmpty().then(() => setReady(true));
+    (async () => {
+      await seedCategoriesIfEmpty();
+      const onboarded = await getHasOnboarded();
+      setShowOnboarding(!onboarded);
+      setReady(true);
+    })();
   }, []);
 
   if (!ready) return null;
+
+  if (showOnboarding) {
+    return <OnboardingScreen onDone={() => setShowOnboarding(false)} />;
+  }
 
   return (
     <NavigationContainer>
